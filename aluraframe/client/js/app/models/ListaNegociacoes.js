424f947e2e7c6a00a48c3ce36c693d1b96e4da28
yagamikisuke <fabio.evangelion.guitar@gmail.com>
2019-06-02 23:36:29 -0300

    Aplicando padrão Proxy

--- aluraframe/client/js/app/models/ListaNegociacoes.js
+++ aluraframe/client/js/app/models/ListaNegociacoes.js
@@ -1,14 +1,11 @@
 class ListaNegociacoes{
 
-    constructor(armadilha){
+    constructor(){
         this._negociacoes = [];
-        this._armadilha = armadilha;
         }
 
     adiciona(negociacao) {
         this._negociacoes.push(negociacao);
-        //Reflect.apply(this._armadilha, this._contexto, [this]);
-        this._armadilha(this);
     }
 
     get negociacoes() {
@@ -18,7 +15,5 @@ class ListaNegociacoes{
     esvazia() {
         
         this._negociacoes = [];
-        //Reflect.apply(this._armadilha, this._contexto, [this]);
-        this._armadilha(this);
     }
 }
