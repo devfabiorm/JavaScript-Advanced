6be3c782e7d84ace5a7dea57249d8b07e7247361
yagamikisuke <fabio.evangelion.guitar@gmail.com>
2019-06-04 03:48:32 -0300

    Aplicando padrão Factory e criando Bind do modelo com a View

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -6,28 +6,13 @@ class NegociacaoController {
         this._inputQuantidade = $("#quantidade");
         this._inputValor = $("#valor");
 
-        self = this;
-        this._listaNegociacoes = new Proxy(new ListaNegociacoes(), {
-
-            get(target, prop, receiver){
-
-                if(['adiciona', 'esvazia'].includes(prop) && typeof(target[prop]) == typeof(Function)){
-                    
-                    return function(){
-                        Reflect.apply(target[prop], target, arguments);
-                        self._negociacoesView.update(target);
-                    }
-                }
-            return Reflect.get(target, prop, receiver);
-            }
-        });
-        
         this._negociacoesView = new NegociacoesView($("#negociacoesView"));
-        this._negociacoesView.update(this._listaNegociacoes);
-
-        this._mensagem = new Mensagem();
+        this._listaNegociacoes = new Bind(new ListaNegociacoes(), this._negociacoesView, ['adiciona', 'esvazia']);
+        
+        
+        
         this._mensagemView = new MensagemView($("#mensagemView"));
-        this._mensagemView.update(this._mensagem);
+        this._mensagem = new Bind(new Mensagem(), this._mensagemView, ['texto']);
 
     }
 
@@ -37,7 +22,7 @@ class NegociacaoController {
         
         this._listaNegociacoes.adiciona(this._criaNegociacao());
         this._mensagem.texto = "Negociação adicionada com sucesso.";
-        this._mensagemView.update(this._mensagem);
+        
         this._limpaFormulario();
     }
 
